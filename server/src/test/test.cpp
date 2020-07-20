4dbd52bcf435d167b613e996d252f4a67a457602
ontes <superpiero23@hotmail.it>
2020-07-20 12:50:47 +0200

    Fixed mysql CRUD test and inserted the INSERT test, and cleaning function at the end

--- server/src/test/test.cpp
+++ server/src/test/test.cpp
@@ -10,32 +10,52 @@ void printResult(int tests, int errors)
 
 void testSQLCRUD()
 {
-    // test select
+
     std::clog << "##### TEST SQL CRUD #####\n";
     int tests = 2;
-    int errors = 2;
+    int errors = 1;
     std::unique_ptr<sql::PreparedStatement> stmt;
     std::unique_ptr<sql::ResultSet> res;
-    std::shared_ptr<sql::Connection> con = DBConnect::getConnection();
+    std::shared_ptr<sql::Connection> con;
+    
+    // test insert
+    con = DBConnect::getConnection();
+    stmt = std::unique_ptr<sql::PreparedStatement>{std::move( con->prepareStatement("INSERT INTO users(id, username, password, salt) VALUES(DEFAULT, ?, ?, ?)"))};
+    stmt->setString(1, "Test_username");
+    stmt->setString(2, "0001");
+    stmt->setInt(3, 5);
+    stmt->execute();
+    
+    // test select
+    con = DBConnect::getConnection();
     stmt = std::unique_ptr<sql::PreparedStatement>{std::move( con->prepareStatement("SELECT username FROM users WHERE username = ?"))};
-    stmt->setString(1, "Pippo");
+    stmt->setString(1, "Test_username");
     res = std::unique_ptr<sql::ResultSet>{std::move(stmt->executeQuery())};
     res->next();
     std::string username = std::string { res->getString("username").c_str() };
     std::clog << "username: " << username << "\n";
-    if (username.compare("Pippo") != 0) {
-        errors++;
-        std::clog << "got: " << username << ", expected: Pippo\n";
-    }
-    // test insert
-    con = DBConnect::getConnection();
-    stmt = std::unique_ptr<sql::PreparedStatement>{std::move( con->prepareStatement("INSERT INTO users(id, username, password, salt) VALUES(DEFAULT, ?, ?, ?)"))};
-    stmt->setString(1, "Pluto");
-    stmt->setString(2, "0001");
-    stmt->setInt(3, 5);
-    stmt->execute();
+    if (username.compare("Test_username") != 0) 
+        std::clog << "got: " << username << ", expected: Test_username\n";
+    else
+    	errors--;
+    
+
+
+
+   
+    
+    // end test function
     printResult(tests, errors);
+    
+    // clean tests queries
+    con = DBConnect::getConnection();
+    stmt = std::unique_ptr<sql::PreparedStatement>{std::move( con->prepareStatement("DELETE FROM users WHERE username = ?"))};
+    stmt->setString(1, "Test_username");
+    stmt->execute();
+    
 }
+
+
 
 void testAll()
 {
