0109ea03aac9b561ee5330d6ee446cea4ce5caa7
ontes <superpiero23@hotmail.it>
2020-07-22 09:50:12 +0200

    solved issue related to the log message, and update the snippet file of creation of a database

--- server/include/common/logger.hpp
+++ server/include/common/logger.hpp
@@ -4,11 +4,13 @@
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include <cstring>
 
 class Logger {
 public:
   static std::string log(const std::string &message) {
     std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    return std::string{std::ctime(&time)} + std::string{"\r\r: "} + message;
+    std::string ret_str {std::string{std::strtok(std::ctime(&time), "\n")} + std::string{"\r\r: "} + message};
+    return std::string{std::strtok(std::ctime(&time), "\n")} + std::string{" : "} + message;
   }
 };
