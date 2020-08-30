69028da937f9295462712612d826bde4106b6612
ontes <superpiero23@hotmail.it>
2020-08-30 12:29:30 +0200

    Risolti alcuni problemi in scrittura del file, ma rimane il problema della sovrascrittura. Creato vettore chunks_already_present

--- server/include/filesystem/client_struct.hpp
+++ server/include/filesystem/client_struct.hpp
@@ -18,6 +18,7 @@ private:
     bool file_found=false;
     bool chunk_found=false;
     int index=0;
+    std::vector<int> chunks_already_present;
     std::unique_ptr<json> structure;
 
 
@@ -57,8 +58,8 @@ public:
                 if (tmp["path"].get<std::string>().compare(path) ==
                     0) { // C'è già il path nel client-struct.json
                     file_found = true;
-                    break;
                     std::clog << "file trovato\n";
+                    break;
                 }
                 index++;
             }
@@ -71,19 +72,30 @@ public:
 
         json chunk;
         chunk["id"] = chunk_id;
-        chunk["hash"] = chunk_hash; //todo: ok calcolare l'hash dal blocco di dati, ma confrontarlo con l'hash ricevuto
-        /*bool found_chunk = false;
+        chunk["hash"] = chunk_hash;
+        (*structure)["entries"][index]["chunks"].push_back(chunk);
+
         for (size_t i = 0; i < (*structure)["entries"][index]["chunks"].size();
              i++) {
-            if ((*structure)["entries"][index]["chunks"][i]["id"] == chunk["id"]) {
-                found_chunk = true;
-                (*structure)["entries"][index]["chunks"][i] = chunk;
+            std::string ind = (*structure)["entries"][index]["chunks"][i]["id"];
+            chunks_already_present.push_back(std::stoi(ind));
+        }
+
+        std::ofstream outfile;
+        outfile.open("../../filesystem/"+username+"/"+file_path);
+        int full_chunk_size = 50;
+
+        std::vector<char> padding(full_chunk_size, '0'); //todo: recuperare la vera chunk size
+        for(int i = 0 ; i < std::stoi(chunk_id) ; i++){
+            if(!std::count(chunks_already_present.begin(),chunks_already_present.end(),i)){ //cerco se quel chunk_id è già stato scritto, se non lo è, devo 0 fillare
+                for(auto p : padding)
+                    outfile << p;
             }
         }
-        if (!found_chunk) {
-         */
-            (*structure)["entries"][index]["chunks"].push_back(chunk);
+        for(auto b : chunk_body)
+            outfile << b;
 
+        outfile.close();
         //}
 
         (*structure)["entries"][index]["size"] = -1; //todo: mettere il file size senzato, quando capirò come creare effettivamente i files dal chunk
@@ -117,7 +129,7 @@ public:
     }
 
 
-    void created_new_file(std::string username, std::string chunk_hash, std::string chunk_id, std::string path, std::string file_size, bool validity, std::string chunk_size, std::string timestamp_locale, std::vector<char> chunk_body){
+    void created_new_file(std::string username, std::string chunk_hash, std::string chunk_id, std::string file_path, std::string file_size, bool validity, std::string chunk_size, std::string timestamp_locale, std::vector<char> chunk_body){
         json entry;
         json chunk;
         std::clog << "e5\n";
@@ -126,7 +138,7 @@ public:
         chunk["hash"] = chunk_hash;
 
         entry["chunks"].push_back(chunk);
-        entry["path"] = path;
+        entry["path"] = file_path;
         entry["size"] = file_size; //todo: mettere il file size senzato, quando capirò come creare effettivamente i files dal chunk
         entry["validity"] = validity;
         entry["dim_last_chunk"] = chunk_size;
@@ -134,17 +146,23 @@ public:
         std::clog << "e6\n";
 
         std::ofstream outfile;
-        outfile.open("../../filesystem/"+username+"/"+path);
-
+        outfile.open("../../filesystem/"+username+"/"+file_path);
+/*
+        std::fstream s(my_file_path); // use option std::ios_base::binary if necessary
+        s.seekp(position_of_data_to_overwrite, std::ios_base::beg);
+        s.write(my_data, size_of_data_to_overwrite);
+        */
         std::vector<char> padding(full_chunk_size, '0'); //todo: recuperare la vera chunk size
-        for(int i = 0 ; i <= std::stoi(chunk_id) ; i++){
-            if(i == std::stoi(chunk_id))
-                for(auto b : chunk_body)
-                outfile << b;
-            else{
-                outfile << padding.data();
+
+        for(int i = 0 ; i < std::stoi(chunk_id) ; i++){
+            if(i != std::stoi(chunk_id)){
+                for(auto b : padding)
+                    outfile << b;
             }
         }
+        for(auto b : chunk_body)
+        outfile << b;
+
         outfile.close();
 
 
