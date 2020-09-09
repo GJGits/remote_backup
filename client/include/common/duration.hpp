7fed902ad681fc159e0236ffaa56702947ee1d1a
GJCode <golla.programmazione@gmail.com>
2020-09-09 12:42:18 +0200

    il master compila, risolti problemi divini

---
+++ client/include/common/duration.hpp
@@ -0,0 +1,18 @@
+#ifndef DURATION_LOGGER
+#define DURATION_LOGGER
+
+#include <string.h>
+#include <time.h>
+#include <iostream>
+#include "color.hpp"
+
+class DurationLogger {
+    private:
+        char *name;
+        int t0;
+    public:
+        DurationLogger(const char *name);
+        ~DurationLogger();
+};
+
+#endif
