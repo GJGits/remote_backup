c997aa381e8a8dd28528feecc684c9606608e5ba
ontes23 <superpiero23@hotmail.it>
2020-10-06 21:38:40 +0200

    Server refactoring (#44)
    * bel commit col device id
    
    * funzionano login e signup

--- server/include/common/jwt.hpp
+++ server/include/common/jwt.hpp
@@ -48,7 +48,7 @@ public:
 
   static std::string generateToken(const Subject &sub, int expiration) {
     json jwt_payload = {
-        {"sub", sub.get_sub()}, {"db", sub.get_db_id()},  {"exp", expiration}};
+        {"sub", sub.get_sub()}, {"db", sub.get_db_id()}, {"device_id", sub.get_device_id()} ,  {"exp", expiration}};
     std::string payload = macaron::Base64::Encode(jwt_payload.dump());
     std::string to_sign{getInstance()->token_header + "." + payload};
     const unsigned char *text = (const unsigned char *)to_sign.c_str();
@@ -58,8 +58,7 @@ public:
     HmacSha256::hmac_sha256(
         text, text_len, (const unsigned char *)getInstance()->secret.c_str(),
         (int)getInstance()->secret.size(), kdigest);
-    json jwt_sign;
-    jwt_sign["sign"] = macaron::Base64::Encode(std::string{kdigest});
+    json jwt_sign{"sign",macaron::Base64::Encode(std::string{kdigest})};
     std::string sign = macaron::Base64::Encode(jwt_sign.dump());
     return getInstance()->token_header + "." + payload + "." + sign;
   }
@@ -84,7 +83,7 @@ public:
             std::vector<std::string> token_parts = Utility::split(token, '.');
           if (token_parts.size() == 3) {
             json payload = json::parse(macaron::Base64::Decode(token_parts[1]));
-            Subject sbj{payload["sub"], (size_t)payload["db"].get<int>()};
+            Subject sbj{payload["sub"], (size_t)payload["db"].get<int>(), (size_t)payload["device_id"].get<int>()};
             int exp = payload["exp"];
             std::string sign =
                 json::parse(macaron::Base64::Decode(token_parts[2]))["sign"];
