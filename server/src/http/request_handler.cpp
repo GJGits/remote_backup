fe50d0a08e6e3965f947af3192575ab5ac803453
ontes23 <superpiero23@hotmail.it>
2020-10-13 10:41:11 +0200

    Implementati i tests signin, aggiustati quelli signup, ed aggiunto test signup con username ripetuto

--- server/src/http/request_handler.cpp
+++ server/src/http/request_handler.cpp
@@ -28,7 +28,7 @@ void request_handler::handle_request(const request &req, reply &rep) {
 
   } catch (UsernameAlreadyExists &e) {
     rep = MakeReply::make_1line_jsonReply<std::string>(
-        "error", e.what(), http::server::reply::internal_server_error);
+        "error", e.what(), http::server::reply::bad_request);
     return;
 
   } catch (CredentialsNotValidException &e) {
